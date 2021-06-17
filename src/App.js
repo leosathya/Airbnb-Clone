cd9930f83fad095d1c8600e48e9355b38b720e7b
sathya <leosathya10@gmail.com>
2021-06-17 19:37:00 +0530

    Home Page Complete with Room card and footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Home from './components/Home';
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="app">
       <Header />
       <Home />
+      <Footer />
       {/* Home */}
         {/* Header */}
         {/* Banner */}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,58 @@
 import React from 'react'
 import './Home.css'
 import Banner from './Banner'
+import Card from './Card'
 
 function Home() {
     return (
         <div className="home">
             <Banner />
-            <div className="home__Seaction">
-                
+            <div className="home__section">
+                <Card 
+                    src="/images/br3.jpg"
+                    title="Penthouse in London"
+                    description="Enjoy the amazing sights of London with
+                    this stunning penthouse."
+                    price="$350/night"
+                />
+                <Card 
+                    src="/images/br3.jpg"
+                    title="Penthouse in London"
+                    description="Enjoy the amazing sights of London with
+                    this stunning penthouse."
+                    price="$350/night"
+                />
+                <Card 
+                    src="/images/br3.jpg"
+                    title="Penthouse in London"
+                    description="Enjoy the amazing sights of London with
+                    this stunning penthouse."
+                    price="$350/night"
+                />
+            </div>
+            <div className="home__section">
+                <Card 
+                    src="/images/br3.jpg"
+                    title="Penthouse in London"
+                    description="Enjoy the amazing sights of London with
+                    this stunning penthouse."
+                    price="$350/night"
+                />
+                <Card 
+                    src="/images/br3.jpg"
+                    title="Penthouse in London"
+                    description="Enjoy the amazing sights of London with
+                    this stunning penthouse."
+                    price="$350/night"
+                />
+                <Card 
+                    src="/images/br3.jpg"
+                    title="Penthouse in London"
+                    description="Enjoy the amazing sights of London with
+                    this stunning penthouse."
+                    price="$350/night"
+                />
+ 
             </div>
         </div>
     )
